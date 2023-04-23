653b40e9a33790337a7bd263508c656d1e434a83
adhamhassan99 <49523561+adhamhassan99@users.noreply.github.com>
2023-04-23 21:48:30 +0200

    adjust action sheet

--- src/sheets/BottomTabSheet.tsx
+++ src/sheets/BottomTabSheet.tsx
@@ -1,15 +1,50 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
 import React from 'react';
-import ActionSheet, {SheetProps} from 'react-native-actions-sheet';
+import ActionSheet, {
+  SheetManager,
+  SheetProps,
+} from 'react-native-actions-sheet';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const BottomTabSheet = (props: SheetProps) => {
   return (
-    <ActionSheet containerStyle={{flex: 0.2}} id={props.sheetId}>
-      <View>
-        <Text>Hello World</Text>
+    <ActionSheet containerStyle={styles.sheetContainer} id={props.sheetId}>
+      <View style={styles.row}>
+        <Pressable onPress={() => SheetManager.hide(props.sheetId)}>
+          <MaterialCommunityIcons name="close" color={'white'} size={25} />
+        </Pressable>
+        <View style={styles.centerTextContainer}>
+          <Text style={[styles.white, styles.centerText]}>
+            Start creating now
+          </Text>
+        </View>
       </View>
     </ActionSheet>
   );
 };
 
 export default BottomTabSheet;
+
+const styles = StyleSheet.create({
+  sheetContainer: {
+    flex: 0.15,
+    backgroundColor: '#5f5f5f',
+    paddingTop: 15,
+    paddingLeft: 20,
+  },
+  centerTextContainer: {
+    marginRight: 55,
+    flex: 1,
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  white: {
+    color: 'white',
+  },
+  centerText: {
+    fontWeight: '500',
+    fontSize: 16,
+  },
+});
