f352f07fe711daf6c494894ad86fab064a1c90f2
adhamhassan99 <49523561+adhamhassan99@users.noreply.github.com>
2023-05-05 00:40:35 +0300

    adj profile sign out

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,12 +1,33 @@
-import {View, Button} from 'react-native';
+import {View, Button, Text} from 'react-native';
 import React from 'react';
 import {SignOutGoogle} from '../utilities/GoogleSIgnOut';
+import styled from 'styled-components/native';
+
+const SignOutBtn = styled.Pressable`
+  background-color: ${props => props.theme.colors.backgroundColorInverse};
+  padding: 20px 40px;
+  border-radius: 10px;
+`;
+
+const PageContainer = styled.View`
+  background-color: ${props => props.theme.colors.backgroundColor};
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const BtnText = styled.Text`
+  color: ${props => props.theme.colors.textColorInverse};
+  font-weight: 600;
+  font-size: 20px;
+`;
 
 const Profile = () => {
   return (
-    <View>
-      <Button title={'sign out'} onPress={SignOutGoogle} />
-    </View>
+    <PageContainer>
+      <SignOutBtn style={{elevation: 10}} onPress={SignOutGoogle}>
+        <BtnText>SIgn Out</BtnText>
+      </SignOutBtn>
+    </PageContainer>
   );
 };
 
